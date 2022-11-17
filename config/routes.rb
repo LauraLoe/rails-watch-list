57ff23df1cfe7122a74a46e2605613ae190718f1
Laura Loeschmann <lauraloeschmann@gmx.net>
2022-11-17 18:28:59 +0100

    day 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # See all lists
+  get "lists", to: "lists#index", as: :lists
+
+  # Create a list
+  get "lists/new", to: "lists#new", as: :new_list
+  post "lists", to: "lists#create"
+
+  # See one list
+  get "lists/:id", to: "lists#show", as: :list
+
+  # Create new bookmark
+  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
+  post "lists/:id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+
+  # Delete a bookmark
+  delete "bookmarks/:id", to: "bookmarks#destroy"
+
 end
