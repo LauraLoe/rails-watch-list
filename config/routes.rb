59eab88d44cf2fb3adffe98df002631d4515242e
Laura Loeschmann <lauraloeschmann@gmx.net>
2022-11-18 16:52:33 +0100

    some layout changes + delete option

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,29 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "list#index"
 
-  # See all lists
-  get "lists", to: "lists#index", as: :lists
+  # # See all lists
+  # get "lists", to: "lists#index", as: :lists
 
-  # Create a list
-  get "lists/new", to: "lists#new", as: :new_list
-  post "lists", to: "lists#create"
+  # # Create a list
+  # get "lists/new", to: "lists#new", as: :new_list
+  # post "lists", to: "lists#create"
 
-  # See one list
-  get "lists/:id", to: "lists#show", as: :list
+  # # See one list
+  # get "lists/:id", to: "lists#show", as: :list
 
-  # Create new bookmark
-  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
-  post "lists/:id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+  # # Create new bookmark
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
+  # post "lists/:id/bookmarks", to: "bookmarks#create"
 
-  # Delete a bookmark
-  delete "bookmarks/:id", to: "bookmarks#destroy"
+  # # Delete a bookmark
+  # delete "bookmarks/:id", to: "bookmarks#destroy"
 
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    # resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  # resources :reviews, only: :destroy
 end
